c6aae7d44cdd16470975f0baa55d3876e2b838b3
marto-k <martow17@gmail.com>
2016-04-23 14:03:58 +0300

    add blogchanges

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Post
+from django.contrib import admin
+from .models import Post, Comment
 
+
+admin.site.register(Comment)
 admin.site.register(Post)
